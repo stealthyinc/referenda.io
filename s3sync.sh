16c43b2cad57fae0aa636408ed21f999176d83ef
prabhaav <prabhaav.bhardwaj@gmail.com>
2019-06-05 21:06:40 -0700

    updated wording, added map, etc

--- s3sync.sh
+++ s3sync.sh
@@ -3,10 +3,11 @@ aws s3 sync assets s3://www.referenda.io/assets --delete --cache-control max-age
 aws s3 sync css s3://www.referenda.io/css --delete --cache-control max-age=36000,public --acl public-read
 aws s3 sync js s3://www.referenda.io/js --delete --cache-control max-age=36000,public --acl public-read
 aws s3 sync manual s3://www.referenda.io/manual --delete --cache-control max-age=36000,public --acl public-read
+aws s3 sync mapsvg s3://www.referenda.io/mapsvg --delete --cache-control max-age=36000,public --acl public-read
 aws s3 sync scss s3://www.referenda.io/scss --delete --cache-control max-age=36000,public --acl public-read
 aws s3 sync about s3://www.referenda.io/about --delete --cache-control max-age=36000,public --acl public-read
-aws s3 sync agatha s3://www.referenda.io/agatha --delete --cache-control max-age=36000,public --acl public-read
 aws s3 sync icons s3://www.referenda.io/icons --delete --cache-control max-age=36000,public --acl public-read
 aws s3 cp _open-generator.url s3://www.referenda.io/_open-generator.url --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
 aws s3 cp _first-steps.url s3://www.referenda.io/_first-steps.url --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
+aws s3 cp googled85d78aae9f668aa.html s3://www.referenda.io/googled85d78aae9f668aa.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
 aws s3 cp index.html s3://www.referenda.io/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
